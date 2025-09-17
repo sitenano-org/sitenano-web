e7b71a76a173213ec335535dd77e051528866d34
Luna <luna@example.com>
2025-09-17 14:35:39 +0300

    GitHub Pages için statik export hazırlandı

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  basePath: '/sitenano-web',
+  assetPrefix: '/sitenano-web/',
   images: {
     unoptimized: true,
     dangerouslyAllowSVG: true,
